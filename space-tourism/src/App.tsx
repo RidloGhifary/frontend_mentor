8a50f2dd55e447c951ee9d5f6eb16c9e9cfe9fd5
RidloGhifary <ridloghfry@gmail.com>
2024-03-15 13:28:24 +0700

    setup project

---
+++ space-tourism/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
